184d2d5ff777fabe614282ccb95a3f8fcbd0ba80
beginoffile <cursos.mapatupisaro@gmail.com>
2023-02-27 11:21:25 -0600

    Update project

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 import './styles.css';
+
+const modalContainer = document.getElementById('modal-container');
+const modalShow = document.getElementById('show-modal');
+const modalClose = document.getElementById('close-modal');
+const bookmarkForm = document.getElementById('bookmark-form');
+const websiteNameEl = document.getElementById('website-name');
+const websiteUrlEl = document.getElementById('website-url');
+const bookmarksContainer = document.getElementById('bookmarks-container');
+
+
+function ShowModal(){
+    modalContainer.classList.add('show-modal');
+    websiteNameEl.focus();
+}
+
+modalShow.addEventListener('click',ShowModal);
+modalClose.addEventListener('click',()=> modalContainer.classList.remove('show-modal'));
+window.addEventListener('click',(e)=>(e.target === modalContainer ? modalContainer.classList.remove('show-modal'):false));
